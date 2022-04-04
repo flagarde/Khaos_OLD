55996ff29b32b786578a1ea0ff0d917cb6c25d4a
flagarde <flagarde.programs@pm.me>
2022-04-04 08:28:49 +0800

    style: Format

--- tests/KhaosVersion.test.cpp
+++ tests/KhaosVersion.test.cpp
@@ -4,12 +4,6 @@
 
 #include <doctest/doctest.h>
 
-TEST_CASE("Test KHAOS_VERSION()")
-{
-  CHECK_EQ( (((MAJOR) % 100) * 10000000) + (((MINOR) % 100) * 100000) + ((PATCH) % 100000), KHAOS_VERSION() );
-}
+TEST_CASE("Test KHAOS_VERSION()") { CHECK_EQ((((MAJOR) % 100) * 10000000) + (((MINOR) % 100) * 100000) + ((PATCH) % 100000), KHAOS_VERSION()); }
 
-TEST_CASE("Test KHAOS_VERSION_TWEAK()")
-{
-CHECK_EQ( static_cast<int>(TWEAK), static_cast<int>(KHAOS_VERSION_TWEAK()) );
-}
+TEST_CASE("Test KHAOS_VERSION_TWEAK()") { CHECK_EQ(static_cast<int>(TWEAK), static_cast<int>(KHAOS_VERSION_TWEAK())); }
